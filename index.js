6d4d6109ce379644e0d36425278137abd1f22fdb
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-24 19:06:54 +0530

    log connected servers and every channel  indside it

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const client = new Discord.Client();
 
 client.once("ready", () => {
   console.log(`Connected as ${client.user.tag}`);
+  console.log("Servers: ");
+  client.guilds.cache.forEach((guild) => {
+    console.log(` - ${guild.name}`);
+    guild.channels.cache.forEach((channel) => {
+      console.log(` -- ${channel.name} (${channel.type} - ${channel.id})`);
+    });
+  });
+  const botsGeneral = client.channels.cache.get('769534520163958789') 
 });
 
 client.login(process.env.SECRET);
