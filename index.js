3d45ff49b07ef1ac447d0d8eee6e2fa8ad9b1818
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-24 19:35:34 +0530

    updated to access messages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const prefix = "!";
+
 client.once("ready", () => {
   console.log(`Connected as ${client.user.tag}`);
   console.log("Servers: ");
@@ -11,8 +13,19 @@ client.once("ready", () => {
       console.log(` -- ${channel.name} (${channel.type} - ${channel.id})`);
     });
   });
-  const botsGeneral = client.channels.cache.get('769534520163958789') 
-  botsGeneral.send("Hello, world!")
+  // const botsGeneral = client.channels.cache.get("769534520163958789");
+  // botsGeneral.send("I am back Online\nTalk to me");
 });
 
+client.on("message", (message) => {
+  if (message.author == client.user) {
+    return;
+  }
+  // message.channel.send(
+  //   `Message Received: ${message.content}\nFrom: ${message.author.tag}`
+  // );
+  if (message.content == `${prefix}ping`) {
+    message.channel.send("Pong");
+  }
+});
 client.login(process.env.SECRET);
