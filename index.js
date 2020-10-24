440cd57c0968ba725d0f706462c315d8907fd0f0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-24 20:42:11 +0530

    added feature to search gif
    !gif <name> will give a random gif related to the given name

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const axios = require("axios");
 
 const prefix = "!";
+
+const giphyTranslateEndpoint = "https://api.giphy.com/v1/gifs/translate";
+
+const getGif = async (query) => {
+  const api_url = `${giphyTranslateEndpoint}?api_key=${process.env.GIPHY_KEY}&s=${query}`;
+  const get = axios({ url: api_url, mode: "cors" });
+  return get
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
 
 client.once("ready", () => {
   console.log(`Connected as ${client.user.tag}`);
@@ -16,11 +31,24 @@ client.on("message", (message) => {
   if (message.author == client.user) {
     return;
   }
-  if (message.content == `${prefix}ping`) {
-    message.channel.send("Pong");
+  if (message.content.match(/bye/i)) {
+    message.channel.send(
+      "https://tenor.com/view/bye-slide-baby-later-peace-out-gif-12999722"
+    );
   }
-  if(message.content.match(/bye/i)){
-    message.channel.send('https://tenor.com/view/bye-slide-baby-later-peace-out-gif-12999722')
+  if (message.content.startsWith(`${prefix}gif`)) {
+    const query = message.content.split(" ");
+    const gif = getGif(query[1]);
+    gif
+      .then((gif) => {
+        message.channel.send(gif.data.data.images.original.url);
+      })
+      .catch((error) => {
+        message.channel.send(
+          `Sorry, There was an error fetching gif\n${error}`
+        );
+      });
   }
 });
+
 client.login(process.env.SECRET);
