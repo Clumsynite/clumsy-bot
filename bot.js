4912b0e7b067db47f3fe17ec3fdc19ae12d0af00
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-24 18:40:36 +0530

    renamed to index

--- bot.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.client()
-require('dotenv').config();
-
-client.on('ready', () => {
-  console.log(`Connected as ${client.user.tag}`)
-})
-
-client.login(process.env.SECRET)
